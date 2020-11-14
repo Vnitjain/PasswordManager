b691d9349787111e56a7a16695354c336e5df780
Vinit Jain <jain.vinit14@gmail.com>
2020-11-14 11:31:21 +0530

    Created a new branch for testing

--- app/src/main/java/jain/vinit/passwordmanager/MainActivity.kt
+++ app/src/main/java/jain/vinit/passwordmanager/MainActivity.kt
@@ -2,10 +2,12 @@ package jain.vinit.passwordmanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_main)
     }
 }
