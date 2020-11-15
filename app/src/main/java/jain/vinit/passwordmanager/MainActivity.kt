c23a3219c713c604df952686b26edd24af044121
Vinit Jain <jain.vinit14@gmail.com>
2020-11-15 12:51:39 +0530

    Added random text

--- app/src/main/java/jain/vinit/passwordmanager/MainActivity.kt
+++ app/src/main/java/jain/vinit/passwordmanager/MainActivity.kt
@@ -3,11 +3,14 @@ package jain.vinit.passwordmanager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_main)
+//        val textView = findViewById<TextView>(R.id.textView)
+//        textView.setText("Hello")
     }
 }
