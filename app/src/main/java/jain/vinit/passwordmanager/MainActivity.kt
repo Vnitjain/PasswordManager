8159ee2b8b34082fd10bb1aad683fd139b333487
Vinit Jain <jain.vinit14@gmail.com>
2020-11-18 18:53:31 +0530

    Design changes

--- app/src/main/java/jain/vinit/passwordmanager/MainActivity.kt
+++ app/src/main/java/jain/vinit/passwordmanager/MainActivity.kt
@@ -4,12 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_main)
+        val textView = findViewById<TextView>(R.id.LoginPageLogin)
+        textView.setOnClickListener{
+            Toast.makeText(this,"Hello",Toast.LENGTH_SHORT).show()
+        }
 //        val textView = findViewById<TextView>(R.id.textView)
 //        textView.setText("Hello")
     }
